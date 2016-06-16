993570f272a4cd94c446a3c4e105807f25fbde69
J. Beasley <jeremypbeasley@jbeasley007554.starbucks.net>
2016-06-15 21:01:38 -0700

    fixed grammar

--- dist/master.js
+++ dist/master.js
@@ -9836,7 +9836,7 @@ $(document).ready(function() {
 if (document.documentElement.clientWidth < 800) {
 	$(function(){
 		$(".WelcomeMessage").typed({
-	    strings: ["My son ^200 says that ^100 I ^100 “make stuff ^100 for the ^100 Internet.” ^400 <br>I'll take that."],
+	    strings: ["My son ^200 says that ^100 I ^100 “make stuff ^100 for the ^100 Internet.” ^400 <br>I’ll take that."],
 	    typeSpeed: 20,
 	    showcursor: false,
 	    contentType: 'html',
@@ -9849,7 +9849,7 @@ if (document.documentElement.clientWidth < 800) {
 if (document.documentElement.clientWidth > 800) {
 	$(function(){
 		$(".WelcomeMessage").typed({
-	    strings: ["My son ^200 says that ^100 I ^100 “make stuff ^100 for the ^100 Internet.” ^400 I'll take that."],
+	    strings: ["My son ^200 says that ^100 I ^100 “make stuff ^100 for the ^100 Internet.” ^400 I’ll take that."],
 	    typeSpeed: 20,
 	    showcursor: false,
 	    contentType: 'html',
