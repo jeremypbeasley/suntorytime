ae80a688ed61f4b467140eecf827a8500168ec64
J. Beasley <jeremypbeasley@jbeasley007554.starbucks.net>
2016-09-10 12:38:24 -0700

    added music section

--- dist/master.js
+++ dist/master.js
@@ -9867,3 +9867,38 @@ if (document.documentElement.clientWidth > 800) {
 		});
 	});
 }
+
+
+// MUSIC
+
+function listProducts(sortedlist) {
+    var output = '';
+    $.each(sortedlist, function(i) {
+        output += '<div class="MusicItem">';
+        output += '<div class="MusicCover" style="background-image: url(' + sortedlist[i].content + ')"></div>';
+        output += '<a class="MusicTitle" href="' + sortedlist[i].description + '">' + sortedlist[i].name + '</a>';
+        output += '</div>';
+    });
+    output += '';
+    document.getElementById("MusicContainer").innerHTML = output;
+};
+
+// function sortProducts(array, key) {
+//     return array.sort(function(a, b) {
+//         var x = a[key];
+//         var y = b[key];
+//         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+//     });
+// }
+
+var productsListSorted = "";
+
+function callProducts() {
+    $.getJSON('http://app.dropmark.com/355854.json?key=638be65e149826ec8892&callback=?', function(result) {
+        var productsList = result.items;
+        //productsListSorted = sortProducts(productsList, 'name');
+        listProducts(productsList);
+    });
+}
+
+callProducts();
